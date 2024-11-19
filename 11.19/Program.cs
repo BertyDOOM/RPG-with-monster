fe0d99794e5fb418e5b68522258099e8b6a53a8e
Бертан Павлолиев <S2409010698@sd.uni-vt.bg>
2024-11-19 09:16:23 +0200

    Add project files.

---
+++ 11.19/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _11._19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int xp = 10; int reward = 5; int threshold = 15; bool success = false;
+            Console.WriteLine("Character - " + xp + "/" + threshold + "xp for next level");
+            Console.WriteLine("Attack the monster in front of you? y/n");
+            string izbor = Console.ReadLine();
+            if (izbor == "y")
+            {
+                Console.WriteLine("You killed the mosnter... +" + reward + " expirience!");
+                if (xp + reward >= threshold)
+                {
+                    success = true;
+                }
+                else 
+                {
+
+                }
+            }
+            else if (izbor == "n")
+            {
+                Console.WriteLine("It killed your pacifist ass.");
+            }
+            else
+            {
+                Console.WriteLine("It killed you before you could do anything.");
+            }
+            Console.WriteLine(success);
+           
+        }
+    }
+}
